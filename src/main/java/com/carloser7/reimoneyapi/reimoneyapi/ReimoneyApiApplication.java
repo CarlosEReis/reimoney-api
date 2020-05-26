caa8a36a90e725714590446ace9e5be2d130b250
CarlosReis <carlos.er7@gmail.com>
2020-05-25 21:54:15 -0300

    3.01 Criando o projeto da API

---
+++ src/main/java/com/carloser7/reimoneyapi/reimoneyapi/ReimoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.carloser7.reimoneyapi.reimoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimoneyApiApplication.class, args);
+	}
+
+}

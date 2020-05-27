9d20ad17a8d505b99f4b0a8acf9437223a0dc8c7
CarlosReis <carlos.er7@gmail.com>
2020-05-26 22:33:32 -0300

    3.05 Colecao vazia, o que retornar?

--- src/main/java/com/carloser7/reimoneyapi/reimoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/carloser7/reimoneyapi/reimoneyapi/resource/CategoriaResource.java
@@ -6,6 +6,7 @@ import com.carloser7.reimoneyapi.reimoneyapi.model.Categoria;
 import com.carloser7.reimoneyapi.reimoneyapi.repository.CategoriaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,9 @@ public class CategoriaResource {
   private CategoriaRepository categoriaRepository;
 
   @GetMapping
-  public List<Categoria> listar() {
-    return categoriaRepository.findAll();
+  public ResponseEntity<?> listar() {
+     List<Categoria> categorias = categoriaRepository.findAll();
+    return ResponseEntity.ok(categorias);
   }
 
 }

8639b2cf427cba17a3b2e02796c099551b6cb590
CarlosReis <carlos.er7@gmail.com>
2020-05-27 00:16:27 -0300

    3.06 Cadastrando nova categoria com POST

--- src/main/java/com/carloser7/reimoneyapi/reimoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/carloser7/reimoneyapi/reimoneyapi/resource/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.carloser7.reimoneyapi.reimoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import com.carloser7.reimoneyapi.reimoneyapi.model.Categoria;
 import com.carloser7.reimoneyapi.reimoneyapi.repository.CategoriaRepository;
@@ -8,8 +11,12 @@ import com.carloser7.reimoneyapi.reimoneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/categorias")
@@ -24,4 +31,21 @@ public class CategoriaResource {
     return ResponseEntity.ok(categorias);
   }
 
+  @PostMapping
+  public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+    
+    Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+      .path("/{codigo}")
+      .buildAndExpand(categoriaSalva.getCodigo())
+      .toUri();
+
+    return ResponseEntity.created(uri).body(categoriaSalva);
+  }
+
+  @GetMapping("/{codigo}")
+  public Categoria buscaPeloCodigo(@PathVariable Long codigo)  {
+    return this.categoriaRepository.findById(codigo).orElse(null);
+  }
 }

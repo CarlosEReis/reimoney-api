5b6e2cb222a2dee5a40cc2d182d760ef5f8dfc75
CarlosReis <carlos.er7@gmail.com>
2020-06-08 19:43:45 -0300

    5.03 Desafio: Cadastrando o primeiro lancamento

--- src/main/java/com/carloser7/reimoneyapi/reimoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/carloser7/reimoneyapi/reimoneyapi/resource/LancamentoResource.java
@@ -3,13 +3,21 @@ package com.carloser7.reimoneyapi.reimoneyapi.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.carloser7.reimoneyapi.reimoneyapi.event.RecursoCriadoEvent;
 import com.carloser7.reimoneyapi.reimoneyapi.model.Lancamento;
 import com.carloser7.reimoneyapi.reimoneyapi.repository.LancamentoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +27,16 @@ public class LancamentoResource {
   
   @Autowired
   private LancamentoRepository lancamentoRepository;
+
+  @Autowired
+  public ApplicationEventPublisher publisher;
+
+  @PostMapping
+  public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+    Lancamento lancamentoSalvo = this.lancamentoRepository.save(lancamento);
+    publisher.publishEvent(new  RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+    return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+  }
 
   @GetMapping
   public ResponseEntity<?> listar() {

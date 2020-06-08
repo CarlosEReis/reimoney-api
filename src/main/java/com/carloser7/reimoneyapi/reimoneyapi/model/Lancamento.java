1322c29e479abca5d30f8f73e9f889e2fe8c676e
CarlosReis <carlos.er7@gmail.com>
2020-06-08 20:29:31 -0300

    5.05 Validando lancamento com Bean Validation

--- src/main/java/com/carloser7/reimoneyapi/reimoneyapi/model/Lancamento.java
+++ src/main/java/com/carloser7/reimoneyapi/reimoneyapi/model/Lancamento.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "lancamento")
@@ -21,18 +22,23 @@ public class Lancamento {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long codigo;
+
+  @NotNull
   private String descricao;
   
+  @NotNull
   @Column(name = "data_vencimento")
   private LocalDate dataVencimento;
 
   @Column(name = "data_pagamento")
   private LocalDate dataPagamento;
 
+  @NotNull
   private BigDecimal valor;
 
   private String observacao;
 
+  @NotNull
   @Enumerated(EnumType.STRING)
   private TipoLancamento tipo;
 
@@ -40,6 +46,7 @@ public class Lancamento {
   @JoinColumn(name = "codigo_categoria")
   private Categoria categoria;
 
+  @NotNull
   @ManyToOne
   @JoinColumn(name = "codigo_pessoa")
   private Pessoa pessoa;
